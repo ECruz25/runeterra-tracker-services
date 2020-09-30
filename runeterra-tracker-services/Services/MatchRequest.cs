c4c7af425c1cdd8bc3ec212cc1604c817c305d76
ecruz25 <edwincruz255@gmail.com>
2020-09-30 00:14:55 -0600

    Add servoce to get all matches by account

---
+++ runeterra-tracker-services/Services/MatchRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace runeterra_tracker_services.Services
+{
+    public class MatchesByAccountRequest
+    {
+        public int Id { get; set; }
+    }
+
+    public class MatchByIdRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ runeterra-tracker-services/Services/MatchService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using runeterra_tracker_services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Match = runeterra_tracker_services.Models.Match;
+
+namespace runeterra_tracker_services.Services
+{
+    public class MatchService
+    {
+        private readonly qzdwlleeContext _context;
+        public MatchService(qzdwlleeContext context)
+        {
+            _context = context;
+        }
+        public List<Match> MatchesshByAccount(MatchesByAccountRequest request)
+        {
+            List<Match> matches = _context.Match.Where(m => m.Accountid == request.Id).ToList();
+            return matches;
+        }
+    }
+}
