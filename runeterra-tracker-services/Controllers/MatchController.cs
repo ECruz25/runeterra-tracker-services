87afa800b6d24d70be5aa8a40c0daae9fe4020fd
ecruz25 <edwincruz255@gmail.com>
2020-09-30 00:20:20 -0600

    add service

--- runeterra-tracker-services/Controllers/MatchController.cs
+++ runeterra-tracker-services/Controllers/MatchController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using runeterra_tracker_services.Models;
+using runeterra_tracker_services.Services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,11 +14,14 @@ namespace runeterra_tracker_services.Controllers
     [ApiController]
     public class MatchController : ControllerBase
     {
+        private static readonly qzdwlleeContext _context = new qzdwlleeContext();
+        private readonly MatchService _services = new MatchService(_context);
         // GET api/<MatchController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public List<Match> Get(MatchesByAccountRequest request)
         {
-            return "value";
+            List<Match> response = _services.MatchesshByAccount(request);
+            return response;
         }
 
         // POST api/<MatchController>
